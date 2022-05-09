ce0a73f9202edd0acbcef0642bb76e596de62868
saksham garg <59284238+sakshamceo@users.noreply.github.com>
2022-05-10 00:31:59 +0530

    Update Set Matrix Zeroes.cpp

--- Arrays/Set Matrix Zeroes.cpp
+++ Arrays/Set Matrix Zeroes.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<int>top(matrix[0].size(),1);
-        vector<int>left(matrix.size(),1);
+        unordered_set<int>top;
+        unordered_set<int>left;
         
         for(int i=0;i<matrix.size();i++)
         {
             for(int j=0;j<matrix[0].size();j++)
             {
                 if(matrix[i][j]==0)
-                {top[j]=0;
-                left[i]=0;
+                {top.insert(j);
+                left.insert(i);
                 }
             }
         }
@@ -18,7 +18,7 @@ public:
         {
             for(int j=0;j<matrix[0].size();j++)
             {
-                if(top[j]==0 || left[i]==0)
+                if(top.find(j)!=top.end() || left.find(i)!=left.end())
                 {
                     matrix[i][j]=0;
                 }
