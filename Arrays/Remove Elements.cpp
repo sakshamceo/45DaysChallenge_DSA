fd0164966a3a3dd12040568eed4fea584b7a8dab
saksham garg <59284238+sakshamceo@users.noreply.github.com>
2022-05-10 00:57:04 +0530

    Create Remove Elements.cpp

---
+++ Arrays/Remove Elements.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int cnt=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=val)
+            {
+                swap(nums[cnt++],nums[i]);
+            }
+        }
+        return cnt;
+    }
+};
